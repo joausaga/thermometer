d4c9d080c39c954521a45c39c051a991e08ddcae
joausaga <jorgesaldivar@gmail.com>
2013-05-12 20:19:26 +0200

    Code refactoring.

--- src/com/lemontruck/thermo/ThermoWidget.java
+++ src/com/lemontruck/thermo/ThermoWidget.java
@@ -12,11 +12,13 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.IBinder;
 import android.util.Log;
+import android.view.View;
 import android.widget.RemoteViews;
-import android.widget.Toast;
 
 import com.lemontruck.thermo.exceptions.ApiException;
 import com.lemontruck.thermo.exceptions.LocationException;
@@ -26,6 +28,16 @@ public class ThermoWidget extends AppWidgetProvider
 {
 	private final static String LOG = "com.lemontruck.thermo";
 	public static final String WIDGET_ID_KEY ="thermowidgetid";
+	
+	@Override
+	public void onEnabled(Context context) {
+		RemoteViews updateViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+		updateViews.setViewVisibility(R.id.updating_circle, View.VISIBLE);
+		updateViews.setTextViewText(R.id.location, "Loading...");
+		ComponentName thisWidget = new ComponentName(context, ThermoWidget.class);
+		AppWidgetManager manager = AppWidgetManager.getInstance(context);
+		manager.updateAppWidget(thisWidget, updateViews);
+	}
 	
 	@Override
 	public void onReceive(Context context, Intent intent) {
@@ -40,28 +52,137 @@ public class ThermoWidget extends AppWidgetProvider
 	
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
-						 int[] appWidgetIds) {
+						 int[] appWidgetIds) 
+	{
 		// To prevent any ANR timeouts, we perform the update in a service
         context.startService(new Intent(context, UpdateService.class));
 	}
 	
 	public static class UpdateService extends Service {
 		private static String PREDEFINED_LOCATION = "Trento";
-		private static String PREDEFINED_CLICK_ACTION = "Update";
 		
 		@Override
         public void onStart(Intent intent, int startId) {
-			// Build the widget update for today
-            RemoteViews updateViews = buildUpdate(this);
-
-            // Push update for this widget to the home screen
-            ComponentName thisWidget = new ComponentName(this, ThermoWidget.class);
-            AppWidgetManager manager = AppWidgetManager.getInstance(this);
-            manager.updateAppWidget(thisWidget, updateViews);
+			RemoteViews updateViews = null;
+			if (existNetworkConnection(this)) {
+				updateViews = showUpdatingCircle(this);
+				doUpdate(this,updateViews); //Making the updating circle appears
+				updateViews = buildUpdateWithNC(this,updateViews);
+			}
+			else {
+				updateViews = buildUpdateWithoutNC(this);
+			}
+			
+			bindActionOnClick(this,updateViews);
+			doUpdate(this, updateViews);
         }
 		
+		public void doUpdate(Context context, RemoteViews views) {
+			ComponentName thisWidget = new ComponentName(context, ThermoWidget.class);
+			AppWidgetManager manager = AppWidgetManager.getInstance(context);
+			manager.updateAppWidget(thisWidget, views);
+		}
+		
+		private void bindActionOnClick(Context context, RemoteViews views) {
+			// When a user presses on the widget it shows a dialog window 
+			// asking him/her to choose what he/she wants to do
+			Intent intent = new Intent(this, ThermoDialog.class);
+			PendingIntent pending = PendingIntent.getActivity(this, 0, intent, 0);
+			views.setOnClickPendingIntent(R.id.widget, pending);
+		}
+		
+		public RemoteViews buildUpdateWithoutNC(Context context) {
+			RemoteViews updateViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+			updateViews = updateViewsVisibility(updateViews, View.INVISIBLE);
+			Resources res = context.getResources();
+			updateViews.setTextViewText(R.id.location, res.getString(R.string.no_network_message));
+			return updateViews;
+		}
+		
+		private RemoteViews showUpdatingCircle(Context context) {
+			RemoteViews updateViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+			updateViews.setViewVisibility(R.id.temp_info, View.GONE);
+			updateViews.setViewVisibility(R.id.updating_circle, View.VISIBLE);
+			return updateViews;
+		}
+		
+		public RemoteViews buildUpdateWithNC(Context context, RemoteViews views) {
+			Resources res = context.getResources();
+			HashMap<String,String> infoLocation = null;
+			
+			Calendar cal = Calendar.getInstance();
+        	SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        	String updateTime = sdf.format(cal.getTime());
+			
+			try {
+				infoLocation = getLocation(res, PREDEFINED_LOCATION);
+			} catch (LocationException e) {
+				views = hideUpdatingCircle(views);
+				Log.e(LOG, e.getMessage());
+			}
+	        String idLocation = infoLocation.get("id_location");
+			String idLastUpdated = infoLocation.get("id_last_updated");
+			String sourceInfoURL = res.getString(R.string.info_source);
+			
+			HashMap<String, String> weatherInfo = null;
+			try {
+                // Try retrieving the weather info
+                WeatherInfoHelper.prepareUserAgent(context);
+                weatherInfo = WeatherInfoHelper.getWeatherInfo(sourceInfoURL,idLocation, idLastUpdated);
+            } catch (ApiException e) {
+            	views = hideUpdatingCircle(views);
+                Log.e(LOG, "Couldn't the weather info source", e);
+            } catch (ParseException e) {
+            	views = hideUpdatingCircle(views);
+                Log.e(LOG, "Couldn't parse the weather info", e);
+            }
+			
+			views = updateViewsVisibility(views, View.VISIBLE);
+			views = updateWidgetViews(views,weatherInfo.get("temp"),PREDEFINED_LOCATION,
+							  		  updateTime,
+							  		  res.getStringArray(R.array.temperature_descriptions));
+            views = hideUpdatingCircle(views);
+            
+			return views;
+		}
+		
+		private RemoteViews updateWidgetViews(RemoteViews views, String temperature, 
+											  String location, String updateTime,
+											  String[] tempDescriptions) 
+		{
+			views.setTextViewText(R.id.temp_info, temperature + "\u00B0");
+            views.setTextViewText(R.id.location, location);
+            views.setTextViewText(R.id.last_updated, "Last Updated " + updateTime);
+            views = updateTempIconAndDesc(views, temperature, 
+            					  		  tempDescriptions);
+            return views;
+		}
+		
+		private boolean existNetworkConnection(Context context) {
+			ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+			NetworkInfo ni = cm.getActiveNetworkInfo();
+			if (ni == null)
+				return false;
+			else
+				return true;
+		}
+		
+		private RemoteViews updateViewsVisibility(RemoteViews views, Integer visibility) {
+			views.setViewVisibility(R.id.temp_icon, visibility);
+			views.setViewVisibility(R.id.temp_desc, visibility);
+			views.setViewVisibility(R.id.temp_info, visibility);
+			views.setViewVisibility(R.id.last_updated, visibility);
+			return views;
+		}
+		
+		private RemoteViews hideUpdatingCircle(RemoteViews views) {
+			views.setViewVisibility(R.id.updating_circle, View.GONE);
+			return views;
+		}
+		
 		private HashMap<String,String> getLocation(Resources res, String locSelected) 
-		throws LocationException {
+		throws LocationException 
+		{
 			HashMap<String,String> infoLocation = new HashMap<String,String>();
 			String[] locations = res.getStringArray(R.array.locations);
 			int indexLocation = -1;
@@ -110,71 +231,7 @@ public class ThermoWidget extends AppWidgetProvider
 			return infoLocation;
 		}
 		
-		public RemoteViews buildUpdate(Context context) {
-			Resources res = context.getResources();
-			HashMap<String,String> infoLocation = null;
-			
-			Calendar cal = Calendar.getInstance();
-        	SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-        	String updateTime = sdf.format(cal.getTime());
-			
-			try {
-				infoLocation = getLocation(res, PREDEFINED_LOCATION);
-			} catch (LocationException e) {
-				Log.e(LOG, e.getMessage());
-			}
-	        String idLocation = infoLocation.get("id_location");
-			String idLastUpdated = infoLocation.get("id_last_updated");
-			String sourceInfoURL = res.getString(R.string.info_source);
-			
-			HashMap<String, String> weatherInfo = null;
-			try {
-                // Try retrieving the weather info
-                WeatherInfoHelper.prepareUserAgent(context);
-                weatherInfo = WeatherInfoHelper.getWeatherInfo(sourceInfoURL,idLocation, idLastUpdated);
-            } catch (ApiException e) {
-                Log.e(LOG, "Couldn't the weather info source", e);
-            } catch (ParseException e) {
-                Log.e(LOG, "Couldn't parse the weather info", e);
-            }
-			
-			// Build an update that holds the updated widget contents
-			RemoteViews updateViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-            updateViews.setTextViewText(R.id.temp_info, weatherInfo.get("temp") + "\u00B0");
-            updateViews.setTextViewText(R.id.location, PREDEFINED_LOCATION);
-            updateViews.setTextViewText(R.id.last_updated, "Last Updated " + updateTime);
-            updateTempIconAndDesc(updateViews, weatherInfo.get("temp"), 
-            					  res.getStringArray(R.array.temperature_descriptions));
-            
-            // When user clicks on widget shows a dialog window asking him/her to choose what he/she wants to do
-            Intent intent = new Intent(context, ThermoDialog.class);
-            PendingIntent pending = PendingIntent.getActivity(context, 0, intent, 0);
-            updateViews.setOnClickPendingIntent(R.id.widget, pending);
-            
-			return updateViews;
-		}
-		
-		private void configureWidgetClick(Context context, RemoteViews views) {
-			PendingIntent pendingIntent = null;
-			if (PREDEFINED_CLICK_ACTION.equalsIgnoreCase("update")) {
-				AppWidgetManager man = AppWidgetManager.getInstance(context);
-			    int[] ids = man.getAppWidgetIds(new ComponentName(context,ThermoWidget.class));
-			    Intent updateIntent = new Intent();
-			    updateIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-			    updateIntent.putExtra(ThermoWidget.WIDGET_ID_KEY, ids);
-			    pendingIntent = PendingIntent.getBroadcast(context, 0, updateIntent, 
-						 								   PendingIntent.FLAG_UPDATE_CURRENT);
-			}
-			else {  /* Visit the source web page */
-				Resources res = context.getResources();
-				String webInfoSource = res.getString(R.string.web_info_source);
-		        Intent defineIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(webInfoSource));
-		        pendingIntent = PendingIntent.getActivity(context, 0, defineIntent, 0);
-			}
-			views.setOnClickPendingIntent(R.id.widget, pendingIntent);
-		}
-		
-		private void updateTempIconAndDesc(RemoteViews updateViews, String strTemp, 
+		private RemoteViews updateTempIconAndDesc(RemoteViews updateViews, String strTemp, 
 										   String[] temp_desc) {
 			Integer temperature = Integer.valueOf(strTemp);
 			if (temperature <= 0) {
@@ -197,6 +254,7 @@ public class ThermoWidget extends AppWidgetProvider
 				updateViews.setImageViewResource(R.id.temp_icon, R.drawable.purple);
 				updateViews.setTextViewText(R.id.temp_desc, temp_desc[4]);
 			}
+			return updateViews;
 		}
 		
 		@Override
