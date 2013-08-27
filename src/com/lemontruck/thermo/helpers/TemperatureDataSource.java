d0172d77c693bf029cf4701f3a7c7ff407dce9d9
joausaga <jorgesaldivar@gmail.com>
2013-08-28 01:35:27 +0200

    Change the function that collect the statistics about the temperatures

--- src/com/lemontruck/thermo/helpers/TemperatureDataSource.java
+++ src/com/lemontruck/thermo/helpers/TemperatureDataSource.java
@@ -3,6 +3,7 @@ package com.lemontruck.thermo.helpers;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -14,6 +15,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.text.format.Time;
 import android.util.Log;
 
+import com.lemontruck.thermo.MainActivity;
 import com.lemontruck.thermo.StatisticFragment;
 import com.lemontruck.thermo.Temperature;
 
@@ -86,33 +88,94 @@ public class TemperatureDataSource {
 	  public List<Temperature> getTemperatures(String country, String city, int filter) {
 		  List<Temperature> temperatures = new ArrayList<Temperature>();
 		  Cursor cursor = null;
-		  String timeCondition = "";
-		  Date date = new Date();
-		  if (filter == StatisticFragment.TODAY) timeCondition = "datetime('now','start of day')";
-		  if (filter == StatisticFragment.THISWEEK) {
-			  SimpleDateFormat formatter = new SimpleDateFormat("u");
-			  Integer numDay = Integer.parseInt(formatter.format(date));
-			  timeCondition = "datetime('now','-" + numDay  + " days')";
-		  }
-		  if (filter == StatisticFragment.THISMONTH) timeCondition = "datetime('now','start of month')";
-		  if (filter == StatisticFragment.THISYEAR) timeCondition = "datetime('now','start of year')";
-		  if (filter == StatisticFragment.THISSEMESTER) {
-			  SimpleDateFormat formatter = new SimpleDateFormat("MM");
-			  Integer numMonth = Integer.parseInt(formatter.format(date));
-			  if (numMonth <= 6) {
-				  timeCondition = "datetime('now','-"+ numMonth +" months')";
-			  }
-			  else {
-				  numMonth = numMonth - 6;
-				  timeCondition = "datetime('now','-"+ numMonth +" months')";
-			  }
-		  }
-		  if (filter == StatisticFragment.SAMEDAYLASTYEAR) timeCondition = "datetime('now','-365 days')";
+		  String startingTime = "";
+		  String endingTime = "";
+		  SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		  Calendar cal = Calendar.getInstance();
 		  
-		  now.setToNow();
-		  String[] args = {country,city,now.toString()};
+		  if (filter != StatisticFragment.SAMEDAYLASTYEAR &&
+			  filter != StatisticFragment.SAMEMONTHLASTYEAR &&
+			  filter != StatisticFragment.SAMEWEEKLASTYEAR) 
+		  {
+			  endingTime = formatter.format(cal.getTime()); // NOW
+			  if (filter == StatisticFragment.THISWEEK) {
+				  Integer currentWeek =  cal.get(Calendar.WEEK_OF_YEAR); 
+				  Integer currentYear =  cal.get(Calendar.YEAR);
+				  cal.clear();
+				  /* Point the calendar to the first day of the current week */
+				  cal.set(Calendar.WEEK_OF_YEAR, currentWeek);
+				  cal.set(Calendar.YEAR, currentYear);  
+			  } 
+			  if (filter == StatisticFragment.THISMONTH) {
+				  /* Point the calendar to the first day of the current month */
+				  cal.set(Calendar.DAY_OF_MONTH, 1);
+			  }
+			  if (filter == StatisticFragment.THISYEAR) {
+				  /* Point the calendar to the first day of the current year */
+				  cal.set(Calendar.DAY_OF_YEAR, 1);  
+			  }
+			  if (filter == StatisticFragment.THISSEMESTER) {
+				  Integer numMonth = cal.get(Calendar.MONTH);;
+				  /* Point the calendar to the first day of the current semester */
+				  if (numMonth <= 6) {
+					  cal.set(Calendar.DAY_OF_YEAR, 1);
+				  }
+				  else {
+					  cal.set(Calendar.MONTH, Calendar.JULY);
+					  cal.set(Calendar.DAY_OF_MONTH, 1);
+				  }
+			  }
+		  } else {
+			  Integer currentYear =  cal.get(Calendar.YEAR);
+			  if (filter == StatisticFragment.SAMEMONTHLASTYEAR) {
+				  /* LAST YEAR */
+				  cal.set(Calendar.YEAR, currentYear-1);
+				  Integer lastDayCurrentMonth = getLastDayMonth(cal);
+				  cal.set(Calendar.DAY_OF_MONTH, lastDayCurrentMonth);
+				  cal.set(Calendar.MINUTE, 59);
+				  cal.set(Calendar.HOUR_OF_DAY, 23);
+				  cal.set(Calendar.SECOND, 59);
+				  endingTime = formatter.format(cal.getTime());
+				  cal.set(Calendar.DAY_OF_MONTH, 1);
+			  }
+			  if (filter == StatisticFragment.SAMEWEEKLASTYEAR) {
+				  Integer currentWeek = cal.get(Calendar.WEEK_OF_YEAR);
+				  /* Point the calendar to the last day and hour of the week */
+				  cal.clear();
+				  cal.set(Calendar.YEAR, currentYear-1);
+				  cal.set(Calendar.WEEK_OF_YEAR, currentWeek+1);
+				  cal.add(Calendar.DATE, -1);
+				  cal.set(Calendar.MINUTE, 59);
+				  cal.set(Calendar.HOUR_OF_DAY, 23);
+				  cal.set(Calendar.SECOND, 59);
+				  endingTime = formatter.format(cal.getTime());
+				  /* Point the calendar to the first day of the week */
+				  cal.clear();
+				  cal.set(Calendar.YEAR, currentYear-1);
+				  cal.set(Calendar.WEEK_OF_YEAR, currentWeek);
+			  }
+			  if (filter == StatisticFragment.SAMEDAYLASTYEAR) {
+				  /* LAST YEAR */
+				  cal.set(Calendar.YEAR, currentYear-1);
+				  cal.set(Calendar.MINUTE, 59);
+				  cal.set(Calendar.HOUR_OF_DAY, 23);
+				  cal.set(Calendar.SECOND, 59);
+				  endingTime = formatter.format(cal.getTime());
+			  }
+		  }
+		  
+		  /* Point the calendar to midnight */
+		  cal.set(Calendar.MINUTE, 0);
+		  cal.set(Calendar.HOUR_OF_DAY, 0);
+		  cal.set(Calendar.SECOND, 0);
+		  //formatter.applyPattern("yyyy/MM/dd HH:mm:ss");
+		  startingTime = formatter.format(cal.getTime());
+		  
+		  Log.i(MainActivity.LOG, "Getting statistics from " + startingTime + " to " + endingTime);
+		  
+		  String[] args = {country,city,startingTime,endingTime};
 		  String whereClause = MySQLiteHelper.COLUMN_COUNTRY + "=? AND " + MySQLiteHelper.COLUMN_CITY + "=? AND " +
- 				  			   MySQLiteHelper.COLUMN_DATETIME + " BETWEEN " + timeCondition + " AND ?";
+ 				  			   MySQLiteHelper.COLUMN_DATETIME + " BETWEEN ? AND ?";
 		  String orderBy = MySQLiteHelper.COLUMN_DATETIME;
 		  cursor = database.query(MySQLiteHelper.TABLE_TEMP,
 			     				  allColumns, 
@@ -139,5 +202,12 @@ public class TemperatureDataSource {
 		  temp.setCountry(cursor.getString(3));
 		  temp.setCity(cursor.getString(4));
 		  return temp;
+	  }
+	  
+	  private Integer getLastDayMonth(Calendar cal) {
+		  cal.add(Calendar.MONTH, 1);  
+	      cal.set(Calendar.DAY_OF_MONTH, 1);  
+	      cal.add(Calendar.DATE, -1);
+	      return cal.get(Calendar.DATE);
 	  }
 }
